6cebe162d6526c90140615492e8c9fda704bfdcb
Long <vuminhl@sheridancollege.ca>
2025-11-23 13:08:56 -0500

    TaskManagement: create enum TaskItem, class TaskStatus, and TaskItemCreatreDTO

---
+++ TaskManagement/TaskManagement.Core/DTOs/TaskItemCreateDTO.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Core.DTOs
+{
+    public class TaskItemCreateDTO
+    {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(200, ErrorMessage = "Title cannot exceed 200 characters")]
+        public string Title { get; set; } = string.Empty;
+        [StringLength(1000, ErrorMessage = "Description cannot exceed 1000 characters")]
+        public string? Description { get; set; }
+        [Required]
+        public Models.TaskStatus Status { get; set; } = Models.TaskStatus.Pending;
+        public DateTime? DueDate { get; set; }
+    }
+}
